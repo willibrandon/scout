898e434d51267b3b04e3bcb37370acae797bc4c4
Brandon Williams <willibrandon@gmail.com>
2026-05-31 08:06:31 -0700

    Fold ignore-file case negation into descriptor

--- src/Scout.App/Flags/Definitions/IgnoreFileCaseInsensitiveFlag.cs
+++ src/Scout.App/Flags/Definitions/IgnoreFileCaseInsensitiveFlag.cs
@@ -4,16 +4,16 @@ namespace Scout.Flags.Definitions;
 
 internal readonly struct IgnoreFileCaseInsensitiveFlag : IFlag<IgnoreFileCaseInsensitiveFlag>
 {
-    public static FlagDescriptor Descriptor { get; } = FlagDescriptor.Switch(
+    public static FlagDescriptor Descriptor { get; } = FlagDescriptor.SwitchWithName(
         "--ignore-file-case-insensitive",
         shortName: null,
         "--no-ignore-file-case-insensitive",
         aliases: [],
         FlagCategory.Search,
         "Match ignore files case insensitively.",
-        static lowArgs =>
+        static (lowArgs, matchedName) =>
         {
-            lowArgs.SetIgnoreFileCaseInsensitive(true);
+            lowArgs.SetIgnoreFileCaseInsensitive(matchedName != "--no-ignore-file-case-insensitive");
             return null;
         });
 }
