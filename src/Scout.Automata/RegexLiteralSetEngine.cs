602f3c80dd1ceb39a85bd457c79fdfc18a50a12c
Brandon Williams <willibrandon@gmail.com>
2026-06-17 15:31:26 -0700

    Prefer packed scanner for small literal sets

--- src/Scout.Automata/RegexLiteralSetEngine.cs
+++ src/Scout.Automata/RegexLiteralSetEngine.cs
@@ -140,6 +140,12 @@ internal sealed class RegexLiteralSetEngine
 
         if (ShouldUseSmallLiteralFinders(this.literals, asciiCaseInsensitive, unicodeCaseInsensitive, useAho))
         {
+            if (RegexPackedLiteralSetScanner.TryCreate(this.literals, out RegexPackedLiteralSetScanner? smallPacked))
+            {
+                packedLiteralScanner = smallPacked;
+                return;
+            }
+
             if (RegexShortLiteralSetScanner.TryCreate(this.literals, out RegexShortLiteralSetScanner? shortScanner))
             {
                 shortLiteralScanner = shortScanner;
